52509f50f14fe8cfe1b0c60ed565b0f71b24f2a4
tlmerbecks <tristanleonard.merbecks@polimi.it>
2026-03-20 21:48:46 +0100

    adding some tests

---
+++ test/test_basics/test_vars_and_pairs.py
@@ -0,0 +1,97 @@
+from pyProp.constants import (
+    pairs,
+    variables,
+    properties
+    )
+from pyProp.utils import (
+    pair_to_vars,
+    vars_to_pair,
+    pair_to_default_pair,
+    var_to_default_var,
+    var_to_property,
+    inputs_to_default_inputs
+    )
+
+
+def test_pair_to_vars():
+
+    pair = pairs.PT
+
+    vars = pair_to_vars(pair)
+    assert vars[0] == variables.P
+    assert vars[1] == variables.T
+
+def test_vars_to_pair():
+
+    var1 = variables.P
+    var2 = variables.T
+
+    pair = vars_to_pair(var1, var2)
+    assert pair == pairs.PT
+
+def test_pair_to_default_pair_TP():
+    
+    pair = pairs.TP
+    dpair = pair_to_default_pair(pair)
+    assert dpair == pairs.PT
+
+def test_pair_to_default_pair_DH():
+    
+    pair = pairs.DmassHmass
+    dpair = pair_to_default_pair(pair)
+    assert dpair == pairs.DmolarHmolar
+
+def test_var_to_default_var():
+
+    var = variables.Dmass
+    dvar = var_to_default_var(var)
+    assert dvar == variables.Dmolar
+
+def test_var_to_property():
+
+    var = variables.P
+    prop = var_to_property(var)
+    assert prop == properties.p
+
+def test_inputs_to_default_inputs_PT():
+
+    # test TP pair
+    pair = pairs.TP
+    vals = (298, 101325)
+
+    dinputs = inputs_to_default_inputs(pair, vals)
+    assert dinputs[0] == pairs.PT
+    
+    dinputs1 = dinputs[1]
+    assert dinputs1["var"] == variables.P
+    assert dinputs1["val"] == 101325
+    assert dinputs1["conv"] == None
+
+    dinputs2 = dinputs[2]
+    assert dinputs2["var"] == variables.T
+    assert dinputs2["val"] == 298
+    assert dinputs2["conv"] == None
+
+def test_inputs_to_default_inputs_SH():
+
+    # test SmolarHmass pair
+    pair  = pairs.SmolarHmass
+    vals = ("s", "h")
+    
+    dinputs = inputs_to_default_inputs(pair, vals)
+    assert dinputs[0] == pairs.HmolarSmolar
+
+    dinputs1 = dinputs[1]
+    assert dinputs1["var"] == variables.Hmolar
+    assert dinputs1["val"] == "h"
+    assert dinputs1["conv"] == 1
+
+    dinputs2 = dinputs[2]
+    assert dinputs2["var"] == variables.Smolar
+    assert dinputs2["val"] == "s"
+    assert dinputs2["conv"] == None  # no conversion is needed as Smolar is already the default var
+
+
+
+
+
---
+++ test/test_coolprop/test_HEOS.py
@@ -0,0 +1,50 @@
+from pyProp import State
+from pyProp.constants import variables, _default_pairs, pairs
+from pyProp.utils import pair_to_vars
+from pyProp.settings import DO_WORKAROUND
+
+import CoolProp as cp
+
+
+def test_HEOS_calc_modes():
+    DO_WORKAROUND = False
+
+    fld = cp.AbstractState("HEOS", "Water")
+    fld.update(cp.PT_INPUTS, 101325, 350)
+
+    vals = {
+        variables.Dmolar: fld.rhomolar(),
+        variables.Hmolar: fld.hmolar(),
+        variables.P: fld.p(),
+        variables.Qmolar: 0.5,
+        variables.Smolar: fld.smolar(),
+        variables.T: fld.T(),
+        variables.Umolar: fld.umolar()
+    }
+
+    unsupported_pairs = (  # CoolProp does not yet support these calculation pairs
+        pairs.HmolarT,
+        pairs.HmolarQmolar,
+        pairs.HmolarUmolar,
+        pairs.QmolarSmolar,  # supposedly supported for Q=0 and Q=1, but does not seem so...
+        pairs.QmolarUmolar,
+        pairs.SmolarUmolar,
+        pairs.TUmolar
+        )
+
+    state = State(fld)
+
+    for pair in _default_pairs:
+
+        var1, var2 = pair_to_vars(pair)
+        val1, val2 = vals[var1], vals[var2]
+
+        try:
+            state.update(pair, val1, val2)
+        except:
+            assert pair in unsupported_pairs
+
+            continue
+
+        
+
